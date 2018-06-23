6ee4948a3e22d25e6c02e58e410df2f384c32ecd
Michael Roy <michaelroy@ellington.local>
2018-06-23 08:37:54 -0600

    Working add of sample PDFs to the view!

--- SampleViewer/ViewController.swift
+++ SampleViewer/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     // Will use a PDFDocument to assign to the PDFView here.
     // @IBOutlet weak var PDFViewingArea: UIView!
     // @IBOutlet weak var PDFPageControl: UIPageControl!
-    @IBOutlet weak var SampleSongImage: UIImageView!
+    @IBOutlet weak var FileNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,20 +32,38 @@ class ViewController: UIViewController {
         v2.transform = CGAffineTransform(rotationAngle: 60 * .pi/180).translatedBy(x: 100, y: 0)
         print(v1.frame)
         // self.view.addSubview(v3)
-        //
+
+        // PDF view
+        let songTitle = "Amazing Grace"
         // bounds of the view area:  PDFViewingArea.bounds
         // of for the current view:  self.view.bounds
         //let pv = UIView(frame:PDFViewingArea.bounds)
         //pv.backgroundColor = UIColor(red: 0.5, green: 0, blue: 0.75, alpha: 0.5)
         // PDFViewingArea.addSubview(pv)
-        let pv: PDFView = PDFView(frame:self.view.bounds.insetBy(dx: 10, dy: 10))
-        //let DocDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask)
-        //let theSong = DocDirectory
+        let mainBundle = Bundle.main
+        // var pv: PDFView = PDFView(frame:self.view.bounds.insetBy(dx: 10, dy: 10))
+        // TODO: try adding a UIView object with constraints, then make this PDFView a subview of it.
+        let pv: PDFView = PDFView(frame:self.view.bounds.insetBy(dx: 15, dy: 30))
+        // from Programming iOS 11, chapter 22, PDFs
+        //let songurl = mainBundle.url(forResource: "Song1", withExtension: "pdf")
+        let otherurl = mainBundle.url(forResource: songTitle, withExtension: "pdf")
+        //let otherurl = mainBundle.url(forResource: songTitle, withExtension: "pdf", subdirectory: "Sample Music")
+        let doc = PDFDocument(url: otherurl!)
+        pv.document = doc
+        pv.displayDirection = PDFDisplayDirection.horizontal
+        pv.usePageViewController(true)
+        // pv.displayMode = PDFDisplayMode.singlePage
+        // pv.autoScales = true
         self.view.addSubview(pv)
+        FileNameLabel.text = songTitle
         
+        /* Display this song as an image.
+        FileNameLabel.text = "Adoramus Te Christe"
         let sampleSong = UIImage(named: "Adoramus Te Christe")
         sampleSong?.resizableImage(withCapInsets: UIEdgeInsets(top:1, left:1, bottom:1, right:1), resizingMode: UIImageResizingMode.stretch)
         SampleSongImage.image = sampleSong
+        */
+        
     }
 
     override func didReceiveMemoryWarning() {
